64e6a5eeea405ad4da7ef5ad6fd9b93e1e8fad53
Nurbol Sagynbekov <nurbolsagynbekov876@gmail.com>
2025-03-21 21:18:13 +0600

    Hello it is my portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -4,28 +4,23 @@ document.querySelector('.header__menu').addEventListener('click', () => {
     mainContent.classList.toggle('main__active');
     sidebar.classList.toggle('me__active');
 });
-
-const menuItems = document.querySelectorAll(".header__link");
-const underline = document.querySelector(".header__underline");
-const first = document.querySelector(".header__link-first")
-
-let f = 0
-function moveUnderline(element) {
-    underline.style.width = `${element.offsetWidth}px`;
-    underline.style.left = `${element.offsetLeft}px`;
-    f=1
-}
-if(!f){
-    moveUnderline(first)
-}
-menuItems.forEach(item => {
-    item.addEventListener("click", (e) => {
-        e.preventDefault();
-        moveUnderline(item);
-    });
+document.querySelector('.me__x').addEventListener('click', () => {
+    const sidebar = document.querySelector('.me');
+    const mainContent = document.querySelector('.main');
+    mainContent.classList.toggle('main__active');
+    sidebar.classList.toggle('me__active');
 });
+document.querySelector('.reload').addEventListener('click', () => {
+    location.reload();
+})
 
-window.addEventListener("load", () => {
-    const activeItem = document.querySelector(".menu-item.active");
-    if (activeItem) moveUnderline(activeItem);
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    const links = document.querySelectorAll('.header__link');
+    links.forEach(link => {
+        const linkPage = link.getAttribute('href').split('/').pop();
+        if (linkPage === currentPage) {
+            link.style.color = "#39965F";
+        }
+    });
 });
